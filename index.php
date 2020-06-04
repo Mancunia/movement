fe522188891a8528aeee60edc17fb3df6aa5ea28
Emmanuel Osei Mensah <mancuniamoe@gmail.com>
2020-06-04 14:02:40 +0000

    Jerry better give me my money

---
+++ index.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>
+Welcome
+</title>
+
+<link href="style/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="style/custom.css" rel="stylesheet" type="text/css">
+
+<script src="js/bootstrap.js"></script>
+
+</head>
+
+
+<body>
+
+<br>
+
+<!-- Page body begin -->
+<main>
+<center>
+<div class="container">
+<div class="card">
+
+
+<div class="card-body">
+<form class="form-group">
+
+<div class="form-group">
+<header align="left">Name</header>
+<div class="row">
+<!--first row -->
+
+<div class="col-3">
+<!--Title -->
+<select class="form-control">
+<option value="">Title</option>
+<option value="">Rev</option>
+<option value="">Dr.</option>
+<option value="">Mr.</option>
+<option value="">Mrs.</option>
+<option value="">Ms.</option>
+</select>
+</div>
+
+<div class="col-3">
+<!--surname_name -->
+<input type="text" placeholder="Surname" class="form-control">
+</div>
+
+<div class="col-3">
+<!--First_name -->
+<input type="text" placeholder="Firstname" class="form-control">
+</div>
+
+<div class="col-3">
+<!--other_name -->
+<input type="text" placeholder="Other name" class="form-control">
+</div>
+
+</div></div><!--first row -->
+<hr><br>
+
+<!-- 2nd row-->
+<div class="form-group">
+<header align="left">Contact</header>
+<div class="row">
+
+<div class="col-3">
+<!--Contact Address -->
+<input type="text" placeholder="Contact Address" class="form-control">
+
+</div>
+
+<div class="col-3">
+<!--Phone -->
+<input type="text" placeholder="Phone #" class="form-control">
+</div>
+
+<div class="col-3">
+<!--House # -->
+<input type="text" placeholder="House #" class="form-control">
+</div>
+
+<div class="col-3">
+<!--Location-->
+<input type="text" placeholder="Locaton" class="form-control" title="City, Town, Region, Country">
+</div>
+
+</div></div><!-- 2nd row-->
+<hr><br>
+<!--3rd row -->
+<div class="form-group">
+<header align="left">Work/Education</header>
+<div class="row">
+<div class="col-3">
+<!--Highest Education -->
+<input type="text" placeholder="Highest Education" class="form-control">
+
+</div>
+
+<div class="col-3">
+<!--Profession -->
+<input type="text" placeholder="Profession" class="form-control">
+</div>
+
+<div class="col-3">
+<!--Occupation -->
+<input type="text" placeholder="Occupation " class="form-control">
+</div>
+
+<div class="col-3">
+<!--Employer-->
+<input type="text" placeholder="Employer" class="form-control" title="City, Town, Region, Country">
+</div>
+
+</div>
+
+</div><!--3rd row -->
+<hr><br>
+<!--4th row -->
+<div class="form-group">
+<header align="left"> Additional Info</header>
+<div class="row">
+
+<div class="col-3">
+<!--Home Town -->
+<input type="text" placeholder="Home Town" class="form-control" >
+</div>
+
+<div class="col-2">
+<!--Region -->
+<input type="text" placeholder="Region" class="form-control" >
+</div>
+
+<div class="col-2">
+<!--Languages -->
+<select type="text"  class="form-control" >
+<option>Languages</option>
+<option>English</option>
+<option>French</option>
+<option>Spanish</option>
+</select>
+</div>
+
+<div class="col-2">
+<!--Religion/Denomination -->
+<input type="text" placeholder="Religion/Denomination" class="form-control" >
+</div>
+
+<div class="col-3">
+<!--Congregation-->
+<input type="text" placeholder="Congregation" class="form-control" >
+</div>
+
+</div>
+
+</div><!--4th row -->
+<hr><br>
+<!--5th row -->
+<div class="form-group">
+<header align="left">About</header>
+<div class="row">
+
+<div class="col-2">
+<!--DOB -->
+<input type="date" placeholder="Home Town" class="form-control" >
+</div>
+
+<div class="col-3">
+<!--Place of birth-->
+<input type="text" placeholder="Place of birth" class="form-control" >
+</div>
+
+<div class="col-2">
+<!--Age-->
+<input type="number" placeholder="Age" class="form-control" >
+</div>
+
+<div class="col-2">
+<!--Sex-->
+<select class="form-control">
+<option value="">Sex</option>
+<option value="">Male</option>
+<option value="">Female</option>
+</select>
+</div>
+
+<div class="col-3">
+<!--Marital Status-->
+<select class="form-control">
+<option value="">Marital status</option>
+<option value="">Married</option>
+<option value="">Single</option>
+<option value="">Widowed</option>
+<option value="">Divorced</option>
+</select>
+</div>
+
+</div>
+
+</div><!--5th row -->
+<hr><br>
+<!--6th row -->
+<div class="form-group">
+<header align="left">My Parents</header>
+
+<div class="row">
+<!-- Father -->
+<div class="col-5">
+<!--Name -->
+<input type="text" placeholder="Name" class="form-control" >
+</div>
+
+<div class="col-2">
+<!--Alive -->
+<select name="" id="" class="form-control">
+<option value="">Alive?</option>
+<option value="">Yes!</option>
+<option value="">No!</option>
+</select>
+</div>
+
+<div class="col-2">
+<!--Age -->
+<input type="number" placeholder="Age" class="form-control" >
+</div>
+
+<div class="col-3">
+<!--Home Town -->
+<input type="text" placeholder="Home Town" class="form-control" >
+</div>
+
+</div>
+<br>
+<div class="row">
+<!-- Mother -->
+<div class="col-5">
+<!--Name -->
+<input type="text" placeholder="Name" class="form-control" >
+</div>
+
+<div class="col-2">
+<!--Alive -->
+<select name="" id="" class="form-control">
+<option value="">Alive?</option>
+<option value="">Yes!</option>
+<option value="">No!</option>
+</select>
+</div>
+
+<div class="col-2">
+<!--Age -->
+<input type="number" placeholder="Age" class="form-control" >
+</div>
+
+<div class="col-3">
+<!--Home Town -->
+<input type="text" placeholder="Home Town" class="form-control" >
+</div>
+</div>
+
+</div><!--6th row -->
+<hr><br>
+
+<div>
+<header align="left">Application & Pledge</header>
+
+<div class="row">
+<p>
+I hereby apply for the membership in the above named movement and agreed to be bonded by it's Articles of 
+Association. I understand that, to join the Movement and help make it Great. I must be committed to <b>PAY MYSELF</b>
+at least <b>One-Tenth (10%)</b> of all <b>Income Earned</b> as <b>Own to Keep</b> and to allow it to be invested in a
+<b>Safe</b> and <b>Secured Investment</b> for <b>Regular Returns.</b> Unless otherwise directed all savings anf the Returns
+thereom, due me, be Rollover with Interest untill My Strength Fails me. To continue to live a meaningful life till I
+leave this planet the returns periodically generated on the Total Investment so far accrued to be paid to me as and when it accrued till i exited this earth, thereafter
+any amount outstanding be paid to my nominees as below unless otherwise directed.
+</p>
+</div>
+<hr>
+
+<p align="center">
+Be willing and /or able to give God what is due Him: Tithe (10%) of income earned. Also pay the following:
+</p>
+
+<p>
+<!-- a-->
+<label for="">(a)</label>
+<b>Membership Guarantee/(Shares)/(Entrance Fees), minimum of GH&cent;200.00 @ GH&cent;10.00 per Guarantee.
+The Minimum Guarantee payment 
+</b> is onetime <b>payment</b> and is subject to change from time to time. The change in Minimum Guarantee Payment shall not have retrospective effect. It may be payable by installments.
+</p>
+<hr>
+<p>
+
+<div class="row">
+<!--b-->
+<div class="col-3">I promise to save at least</div>
+
+<div class="col-3">
+<div class="input-group mb-2">
+        <div class="input-group-prepend">
+          <div class="input-group-text">GH&cent;</div>
+        </div>
+        <input type="number" class="form-control" id="inlineFormInputGroup" placeholder="">
+      </div>
+</div>
+
+<div class="col-6">every Week/Month, being at least (10%) of Income Earned</div>
+ 
+</div>
+
+</p>
+
+
+
+
+
+</div>
+
+
+
+
+<!--Form end -->
+<div class="row">
+
+</div>
+
+</form>
+</div>
+</div>
+
+</div>
+
+
+
+</center>
+
+
+
+
+</main>
+
+<br>
+
+<footer>
+<div class="row">
+
+<div class="col-4">
+<!--1st column -->
+Hello
+</div>
+
+<div class="col-4">
+<!--2nd column -->
+hi
+</div>
+
+<div class="col-4">
+<!--3rd column -->
+Everyone
+</div>
+
+</div>
+</footer>
+</body>
+
+
+</html>
